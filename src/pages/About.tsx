36218b9c2f175b40087d7976b18b033e940d5e8b
Kelly Lin <linkelly150@gmail.com>
2025-11-02 13:50:59 -0500

    final home hero

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,19 +7,22 @@ function About() {
       <Nav />
 
       <div className="hero-content">
+        <div className="hero-main">
+          <p className="hero-title">CALITECH <br></br>SOLUTIONS</p>
+
+          <div className="hero-image">
+            <img src="/about_image.png" alt="Beach scene" />
+          </div>
+        </div>
+
         <div className="hero-text">
-          <h1>CALITECH SOLUTIONS</h1>
           <p>CaliTech Solutions is a Bakersfield-based IT services provider dedicated to helping small and medium-sized businesses thrive through dependable, technical support.</p>
         </div>
 
-        <div className="hero-image">
-          <img src="/about_image.png" alt="Beach scene" />
-        </div>
+        <button className="scroll-button">
+          <img src="/down_arrow.png" alt="Scroll down" />
+        </button>
       </div>
-
-      <button className="scroll-button">
-        <span>↓</span>
-      </button>
     </div>
   )
 }
